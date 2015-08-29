7c96c9199a22c03e113670c60d2f86faf82efc78
Devian <info@devian.tk>
2015-08-29 14:42:50 +0200

    fix bug with browser

--- mdns.js
+++ mdns.js
@@ -1,22 +1,33 @@
 const mdns = Npm.require('mdns')
 
 Mdns = {
-	createAdvertisement: function(serviceType, options) {
-		const advertisement = mdns.createAdvertisement(serviceType, options)
-		var self = {
-			start: Meteor.wrapAsync(advertisement.start),
-			stop:  Meteor.wrapAsync(advertisement.stop )
+	createAdvertisement: mdns.createAdvertisement,
+	createBrowser: function(serviceType, options) {
+		var browser = mdns.createBrowser(serviceType)
+		const self = {
+			on:   function(event, callback) { browser.on(  event, Meteor.bindEnvironment(callback)) },
+			once: function(event, callback) { browser.once(event, Meteor.bindEnvironment(callback)) },
+
+			start: function() { browser.start()},
+			stop:  function() { browser.stop() },
+
+			get domain(     ) { return browser.domain},
+			set domain(value) {browser.domain = value},
+
+			get serviceRef(     ) { return browser.serviceRef},
+			set serviceRef(value) {browser.serviceRef = value},
+
+			get watcher(     ) { return browser.watcher},
+			set watcher(value) {browser.watcher = value}
 		}
 		return self
 	},
-
-	createBrowser: Meteor.wrapAsync(mdns.createBrowser),
-	resolve: Meteor.wrapAsync(mdns.resolve)
+	resolve: Meteor.wrapAsync(mdns.resolve),
 	makeServiceType: mdns.makeServiceType,
 
 	tcp: mdns.tcp,
 	udp: mdns.udp,
 
 	rst: mdns.rst,
-	isAvahi: mdn.isAvahi
+	isAvahi: mdns.isAvahi
 }
